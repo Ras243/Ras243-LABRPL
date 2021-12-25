2ab0e12cf4d8acc0270a3a6b1d2fba207d387147
Ras243 <nurrasuli4@gmail.com>
2021-12-25 01:29:56 -0800

    belajar list

---
+++ day14-01.py
@@ -0,0 +1,11 @@
+# list
+bil =[20,40,50,10,80,30,50,70]
+print(f"angka: {bil}")
+print(f"angka ketiga: {bil[2]}")
+print(f"angka ke....: {bil[-1]}")
+print(f"slicing: {bil[2:5]}")
+
+listkos = [54,7.4,True,'Rasul',['Teknik 21',10]]
+print(f"isi list: {listkos}")
+print(f"isi list kelima: {listkos[4]}")
+print("panjang listkos",len(listkos))
