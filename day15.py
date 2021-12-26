708d1b9130748db39aa6971c10db0fadb6dd8dac
Ras243 <nurrasuli4@gmail.com>
2021-12-26 04:59:28 -0800

    belajar list

---
+++ day15.py
@@ -0,0 +1,30 @@
+ # Tipe data
+
+print("="*7,"string","="*7)
+x = "Belajar ngoding itu mudah, yg susah itu maunya belajar"
+print(x)
+print("tipe data : ",type(x))
+print("="*7,"integer","="*7)
+y = int(15)
+print("nilai : ",y)
+print("tipe data : ",type(y))
+print("="*7,"float","="*7)
+z = float(3.14)
+print("nilai : ",z)
+print("tipe data : ",type(z))
+print("="*7,"boolean","="*7)
+i = 3 < 5
+print(i)
+print("tipe data : ",type(i))
+print("="*7,"list","="*7)
+bhspemrog = ["python","c","C++","java","javascript","php","dark"]
+print(bhspemrog)
+print("tipe data : ",type(bhspemrog))
+print("="*7,"tuple","="*7)
+transportasi = ("sepeda","motor","mobil","bis","kereta api","kapal air","pesawat")
+print(transportasi)
+print("tipe data : ",type(transportasi))
+print("="*7,"range","="*7)
+a = range(6)
+print(a)
+print("tipe data : ",type(a))
