3a26c7cbbd9c2cea569913517c02c267381385f9
Ras243 <nurrasuli4@gmail.com>
2021-12-29 15:45:57 -0800

    belajar set

---
+++ day19.py
@@ -0,0 +1,21 @@
+# set == himpunan
+#'add', 'clear', 'copy', 'difference','difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
+
+superhero = {"wiro sableng","si buta dari gua hantu",
+            "saras 808","gundala",243,"gatot kaca"}
+
+superhero.add("mak lampir")
+superhero.add("gundala")
+print(superhero)
+print()
+# cara lain bikin set
+kue = set()
+
+kue.add("onde-onde")
+kue.add("buroccong")
+kue.add("putu")
+kue.add("jalankote")
+kue.add("onde-onde")
+kue.add("tallo panyyu")
+
+print(sorted(kue))#fungsinya untuk mengurutkan datanya menurut abjad a-z
