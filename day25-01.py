7524878844e8143fd59662d5f04f3094bcab44b9
Ras243 <nurrasuli4@gmail.com>
2022-01-04 17:24:46 -0800

    loop

---
+++ day25-01.py
@@ -0,0 +1,11 @@
+
+i = 1
+
+while i<=5:
+    print("Rasul",end="")
+    j = 4
+    while j<=4:
+        print("bisa",end="")
+        j+=1
+    i+=1
+    print()
---
+++ day25.py
@@ -0,0 +1,21 @@
+
+i = 1
+while i <=10:
+    print("100 Days of coding")
+    i+=1
+    while i >= 10 and i < 20:
+        print("si otong lagi bermain dengan si ucup")
+        i+=2
+else:
+    for i in range(1,5+1):
+        if i == 1:
+            main = "bola"
+        elif i == 2:
+            main = "masak2"
+        elif i == 3:
+            main = "berbie"
+        elif i == 4:
+            main = "robot"
+        elif i == 5:
+            main = "game"
+        print("day",i,"otong ama ucup main apa?\n",f"=> main {main}")
