c7b303641ba8bf41575b54f24028ae892d63b2ea
Ras243 <nurrasuli4@gmail.com>
2022-01-23 04:10:28 -0800

    Day 43

---
+++ day43.py
@@ -0,0 +1,12 @@
+print("Percabangan".center(30,"_"))
+
+presiden = input("Presiden pertama indonesia? : ")
+
+if presiden == "soekarno":
+    print("jawaban anda benar")
+elif presiden == "soeharto":
+    print("Presiden soeharto adalah presiden kedua")
+elif presiden == "jokowi":
+    print("jokowi adalah presiden sekarang")
+else:
+    print("maaf, jawaban anda salah")
