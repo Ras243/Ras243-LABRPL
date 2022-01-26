b3ee44749c05a63da55b9965771815e0aa75f718
Ras243 <nurrasuli4@gmail.com>
2022-01-26 03:53:18 -0800

    Day 46

---
+++ day46.py
@@ -0,0 +1,7 @@
+print("function 3 paramaters, dengan 1 function tdk wajib diisi")
+
+def suhu_udara (daerah, derajat, satuan = 'celcius'):
+  print(f"Suhu di {daerah} adalah {derajat} {satuan}")
+suhu_udara("Majene",30)
+suhu_udara("Pasangkayu",270,"fahrenheit")
+
