d343ff8371b73a3fdf392e7efdd424a3360f0b05
Ras243 <nurrasuli4@gmail.com>
2022-02-02 05:13:14 -0800

    Day 53

---
+++ day53.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'id':
+        print('selamat malan')
+    elif lang == 'fr':
+        print('Rasul')
+    else:
+        print('Halo')
+greet('id')
+greet('fr')
+greet('en')
